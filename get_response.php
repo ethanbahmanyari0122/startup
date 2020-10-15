8a09660b80d70dd8e5b6f7e8e405b2c6d3a9e245
Ethan <70198067+ethanbahmanyari0122@users.noreply.github.com>
2020-10-15 11:03:12 +0200

    php

--- get_response.php
+++ get_response.php
@@ -1,26 +1,29 @@
 <?php
-require_once("config.php");
-if((isset($_POST['lname'])&& $_POST['lname'] !='') && (isset($_POST['email'])&& $_POST['email'] !='')){
-  $lastName = $conn->real_escape_string($_POST['lname']);
-  $firstName = $conn->real_escape_string($_POST['fname']);
-  $phoneNr = $conn->real_escape_integer($_POST['phoneNr']);
-  $email = $conn->real_escape_string($_POST['email']);
-  $city = $conn->real_escape_string($_POST['city']);
-  $plz = $conn->real_escape_integer($_POST['plz']);
-  $houseNr = $conn->real_escape_integer($_POST['houseNr']);
-  $street = $conn->real_escape_string($_POST['street']);
-  $sql="INSERT INTO customer (lastName, firstName,phoneNr, email,city,plz,houseNr,street) VALUES ('".$lastName."','".$firstName."', '".$phoneNr."', '".$email."','".$city."','".$plz."','".$houseNr."','".$street."')";
-  if(!$result = $conn->query($sql)){
-    die('There was an error running the query [' . $conn->error . ']');
-  }
-  else
-  {
-    echo "Thank you! We will contact you soon";
-  }
-  }
 
-else
-{
-  echo "Please fill Name and Email";
+
+// Create connection
+
+$conn = mysqli_connect("localhost","root", "root1234","startup");
+
+
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+    $lastName = $_POST['lastName'];
+    $firstName = $_POST['firstName'];
+    $phoneNr = $_POST['phoneNr'];
+    $email = $_POST['email'];
+    $city = $_POST['city'];
+    $plz = $_POST['plz'];
+    $houseNr = $_POST['houseNr'];
+    $street = $_POST['street'];
+    $sql = "INSERT INTO customer (lastName,firstName,phoneNr,email,city,plz,houseNr,street) VALUES ('". $lastName ."','" . $firstName . "', '" . $phoneNr . "', '" . $email . "','" . $city . "','" . $plz . "','" . $houseNr . "','" . $street . "')";
+    if (!$result = $conn->query($sql)) {
+      die('There was an error running the query [' . $conn->error . ']');
+    } else {
+      echo "Thank you! We will contact you soon";
+
+  }
 ?>
